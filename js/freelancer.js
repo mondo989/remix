1dffe64367638ee05d1c2cca4140080558d42316
Armando <flores@mtu.edu>
2014-06-11 22:48:16 -0700

    Remix of Armando.sx
    Bootstrapping my 2nd Iteration of my Website

---
+++ js/freelancer.js
@@ -0,0 +1,46 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+$('.circle').on("click", function(){
+    $(".demo-row-2").show("slow");
+});
+
+$('#begin-contact').on("click", function (){
+    
+    $("#contact").show("slow");
+    $('#begin-contact').hide();
+
+});
+
+$('#nav-contact').on("click", function (){
+     $("#contact").show("slow");
+     $('#begin-contact').hide();
+});
+
+
+// Floating label headings for the contact form
+$(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+
